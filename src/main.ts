c66b0077c401176c5c8ba43189783c24be52d58f
Quentin Vanbutsele <vanbutsele@intechinfo.fr>
2025-04-11 22:44:07 +0200

    allow loading scene from xml file

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import Scene from './class/scene';
 import './style.css'
 window.customElements.define("game-scene", Scene);
 
-const scene = document.createElement("game-scene") as Scene;
-
-
+//const scene = document.createElement("game-scene") as Scene;
 
 const app = document.querySelector<HTMLDivElement>('#app');
-app!.append(scene);
+
+fetch('/scenes/scene1.xml')
+.then((response) => response.text())
+.then((text) => {
+    app!.innerHTML = text;
+});
+
+//app!.append(scene);
 
